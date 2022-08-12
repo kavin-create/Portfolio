5c37bc2f43989f5d3309c035b9c63d83987bab7a
kavin <srijanrcis@gmail.com>
2022-08-12 23:07:54 +0530

    added files

--- developer-portfolio-template/developer-portfolio-template/js/main.js
+++ developer-portfolio-template/developer-portfolio-template/js/main.js
@@ -94,16 +94,6 @@
     
     
     // Portfolio filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-filter li').on('click', function () {
-        $("#portfolio-filter li").removeClass('filter-active');
-        $(this).addClass('filter-active');
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
     
 })(jQuery);
 
